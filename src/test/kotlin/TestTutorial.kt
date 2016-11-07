00dec3462d9d6ded9e444ff6691a3075728ffc46
linnal <erindajaupi@gmail.com>
2016-11-07 22:32:10 +0100

    added spek tests

---
+++ src/test/kotlin/TestTutorial.kt
@@ -0,0 +1,24 @@
+
+import org.jetbrains.spek.api.Spek
+import kotlin.test.assertEquals
+
+/**
+ * Created by linnal on 11/7/16.
+ */
+class TestTutorial: Spek({
+    describe("a calculator") {
+
+        it("should return the result of adding the first number to the second number") {
+            assertEquals(6, 6)
+        }
+
+        it("should return the result of adding the first number to the second number") {
+            assertEquals(4, 4)
+        }
+
+        it("should return the result of adding the first number to the second number") {
+            assertEquals(4, 4)
+        }
+
+    }
+})
